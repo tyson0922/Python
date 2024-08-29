19876110558976e3e79cb4bf7ead6d7cae7a2bcf
tyson0922 <55447041+tyson0922@users.noreply.github.com>
2024-08-29 21:27:51 +0900

    Add files via upload

---
+++ Code10-01.py
@@ -0,0 +1,9 @@
+
+#3행의 tkinter는 파이썬에서 GUI 관련 모듈을 제공하는 표준 윈도우 라이브러리임
+from tkinter import *
+
+#Tk()는 기본이 되는 윈도우를 반환하는데 이를 루트 윈도우 또는 베이스 윈도우라고 함​
+window = Tk()
+
+#window.mainloop 함수를 실행함​
+window.mainloop()
---
+++ Code10-02.py
@@ -0,0 +1,13 @@
+
+
+from tkinter import *
+
+window = Tk()
+window.title("윈도우창 연습")
+window.geometry("400x100")
+window.resizable(width=False, height=False)
+
+window.mainloop()
+
+# 6행에서 윈도우 창에 제목을 표시하고, 7행에서 윈도우의 초기 크기를 400x100으로 지정함​
+# 8행에서는 가로와 세로의 크기가 변경되지 않도록 지정함
