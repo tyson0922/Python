f4165e8764671b9977c680705ebe643b8d61215f
tyson0922 <55447041+tyson0922@users.noreply.github.com>
2024-08-30 19:32:25 +0900

    Add files via upload

---
+++ Code10-04.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+window = Tk()
+
+photo = PhotoImage(file="gif/dog.gif")
+label1 = Label(window, image=photo)
+
+label1.pack()
+
+window.mainloop()
---
+++ Code10-05.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+window = Tk()
+
+button1 = Button(window, text="파이썬 종료", fg="red",
+                 command=quit)
+
+button1.pack()
+
+window.mainloop()
